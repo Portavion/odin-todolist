5a043d15a4e3e0a2c7277c0acd7a724f1bca8e15
Portavion <156545430+Portavion@users.noreply.github.com>
2024-10-14 14:14:27 +0100

    Create TodoItem class in todo module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import "./style.css";
-import { test } from "./template.js";
+import { TodoItem } from "./todo.js";
+
+
+let test = new TodoItem('test', 'todo pour testeer', '20-03-2025', 1);
+
+console.log(test);
+
+test.title = 'yooooo';
 
 console.log(test);
