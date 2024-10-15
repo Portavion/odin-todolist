ecef903ba9fe38e3118035d749722d974201198a
Portavion <156545430+Portavion@users.noreply.github.com>
2024-10-15 11:18:07 +0100

    Add ui function to display list of projects in the navigation bar

--- src/index.js
+++ src/index.js
@@ -9,15 +9,23 @@ const testTodo = new TodoItem('test', 'todo pour testeer', '20-03-2025', 1);
 const testTodo2 = new TodoItem('test2', 'todo pour testeer', '20-03-2025', 1);
 
 const defaultProject = new Project('Default Project');
+const project1 = new Project('Project 1');
+const project2 = new Project('Project 2');
+
 const workspace = new Workspace('Default Workspace');
 
 workspace.addProject(defaultProject)
+workspace.addProject(project1);
+workspace.addProject(project2);
+
+
 defaultProject.addTodoItem(testTodo);
 defaultProject.addTodoItem(testTodo2);
-
 
 //defaultProject.removeTodo(testTodo2);
 
 console.log(workspace.listProjects());
-console.log(defaultProject.listTodos());
+// console.log(defaultProject.listTodos());
+
+UIController.displayProjects(workspace.listProjects());
 
--- src/ui.js
+++ src/ui.js
@@ -1,7 +1,21 @@
 export { UIController };
 
 const UIController = new class {
-    constructor () {
+    constructor() {}
+  
+    displayProjects (projectList) {
+        const projectContainer = document.querySelector('.my-projects');
+        const projectListContainer = document.createElement('ul');
+        // console.log('List of projects' + projectList);
 
+        for (let projectID in projectList) {
+            let newListItem = document.createElement('li');
+
+            newListItem.appendChild(document.createTextNode(projectList[projectID].name));
+            
+            projectListContainer.appendChild(newListItem);
+        }
+
+        projectContainer.appendChild(projectListContainer);
     }
-};
+}
